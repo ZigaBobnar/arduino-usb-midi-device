5b3c0e0094b2ccabd3eb53e7a2fae108d1779363
Ziga Bobnar <ziga@bobnar.net>
2020-01-10 23:12:28 +0100

    Prepare serial connection, read single matrix element

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,21 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 
 void setup() {
   MIDI.begin(MIDI_CHANNEL);
+
+  Serial.begin(9600);
+
+  pinMode(13, OUTPUT);
+  pinMode(3, INPUT_PULLUP);
 }
 
 void loop() {
+  digitalWrite(13, LOW);
 
+  digitalWrite(13, HIGH);
+
+  Serial.println(digitalRead(3));
+
+  digitalWrite(13, LOW);
+
+  delay(100);
 }
