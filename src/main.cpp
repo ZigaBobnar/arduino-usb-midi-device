2875aef70a8f9b453f34cf77c722c88d698a5fc0
Ziga Bobnar <ziga@bobnar.net>
2020-01-10 23:20:17 +0100

    Add cols and rows mapping to arduino pins and initialize them

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,27 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 
 #define MIDI_CHANNEL 4
 
+const int cols_pins[] = { 30, 40, 42, 44, 46, 48, 50, 52 };
+const int cols_count = 8;
+const int rows_pins[] = { 10, 11, 12, 13 };
+const int rows_count = 4;
+
 void setup() {
   MIDI.begin(MIDI_CHANNEL);
 
   Serial.begin(9600);
 
-  pinMode(13, OUTPUT);
-  pinMode(3, INPUT_PULLUP);
+  for (int i = 0; i < cols_count; ++i) {
+    pinMode(cols_pins[i], OUTPUT);
+  }
+
+  for (int i = 0; i < rows_count; ++i) {
+    pinMode(rows_pins[i], INPUT_PULLUP);
+  }
 }
 
 void loop() {
-  digitalWrite(13, LOW);
 
-  digitalWrite(13, HIGH);
-
-  Serial.println(digitalRead(3));
-
-  digitalWrite(13, LOW);
 
   delay(100);
 }
