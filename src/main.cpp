7a3a82d3802f1fa5f13d410dbb50a2be48a37ee6
Ziga Bobnar <ziga@bobnar.net>
2020-01-10 23:30:24 +0100

    Add key pressed checking logic

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,29 @@
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-#define MIDI_CHANNEL 4
+const int midi_channel = 4;
+const int midi_velocity = 127;
 
 const int cols_pins[] = { 30, 40, 42, 44, 46, 48, 50, 52 };
 const int cols_count = 8;
 const int rows_pins[] = { 10, 11, 12, 13 };
 const int rows_count = 4;
 
+const int key_to_midi_map[cols_count * rows_count] = {
+  // TODO
+  0
+};
+
+boolean previous_states[cols_count * rows_count] = { 0 };
+
 void setup() {
-  MIDI.begin(MIDI_CHANNEL);
+  MIDI.begin(midi_channel);
 
   Serial.begin(9600);
 
   for (int i = 0; i < cols_count; ++i) {
     pinMode(cols_pins[i], OUTPUT);
+    digitalWrite(cols_pins[i], LOW);
   }
 
   for (int i = 0; i < rows_count; ++i) {
@@ -25,7 +34,26 @@ void setup() {
 }
 
 void loop() {
+  for (int i = 0; i < cols_count; ++i) {
+    digitalWrite(cols_pins[i], HIGH);
 
+    for (int j = 0; j < rows_count; ++j) {
+      int readout = digitalRead(rows_pins[j]);
+      int map_index = i * cols_count + j;
+
+      if (readout == HIGH) {
+        if (previous_states[map_index] == false) {
+          // Send note on
+        }
+      } else {
+        if (previous_states[map_index] == true) {
+          // Send note off
+        }
+      }
+    }
+
+    digitalWrite(cols_pins[i], LOW);
+  }
 
   delay(100);
 }
